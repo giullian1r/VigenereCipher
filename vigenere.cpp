29c1eade8c69bac86a16b65a308a8a294e2400ff
ant-linux <antunigiulliano2@gmail.com>
2024-09-23 15:10:42 -0300

    refactored code and implemented new functions

--- vigenere.cpp
+++ vigenere.cpp
@@ -1,79 +1,116 @@
+// check :: colocar o codigo em funcoes 
+// check :: implementar funcionalidade de deixar texto uppercase :
+//          fazer o caminho reverso e implemntar a decodificacao da cifra 
+//          fazer menu para o usuario escolher acao
+
 #include<iostream>
 #include<vector>
 #include<string>    
 using namespace std;
 
-// colocar o codigo em funcoes 
-// implementar funcionalidade de deixar qualquer texto upper case
-// fazer o caminho reverso e implemntar a decodificacao da cifra 
-// fazer menu para o usuario escolher acao
+//global consts  
+const int col = 26;
+const int lin = 26;
 
-
-int main(){
-    const int lin = 26;
-    const int col = 26;
-    char matrix[lin][col];
-    vector<char> chars({'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
-                       'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'});
-
-
-    //preenche matriz
-    for(int i = 0; i < lin; i++){
+//init functions
+void matrix_fill(char (matrix)[lin][col], vector<char> chars){
+ for(int i = 0; i < lin; i++){
         for(int j = 0; j < col; j++){
             matrix[i][j] = chars[(i + j) % chars.size()];
         }        
     }
-    //imprime matriz
+}
+
+//print functions
+void matrix_print(char (matrix)[lin][col]){
     for(int i = 0; i < lin; i++){
         for(int j = 0; j < col; j++){
-            cout << matrix[i][j] << " ";
+            cout << matrix[i][j] << "  ";
         }        
         cout << endl;
     }
+    cout << endl;
+}
 
-    //inputs
-    string t;
-    cout << "digite o que deseja codificar: " << endl;
-    cin >> t;
+void vector_print(vector<char> vec){
+    for(int i = 0; i < vec.size(); i++){
+        cout << vec[i];
+    }
+    cout << endl;
+}
 
-    string k;
-    cout << "digite a sua chave: *deve conter o mesmo numero de caracteres que a sua palavra/frase" << endl;
-    cin >> k;
+//string manipulates
+void toUpperString(string& text){
+     for (char& c : text) {
+        c = toupper(c);
+    }
+}
 
-    vector<char>key({});
-    vector<char>text({});
-
-    //separa string 
+void string_separate(vector<char>& text, string& t, vector<char>& key, string& k){
     for(int i = 0; i < k.length(); i++){
           text.push_back(t[i]);
           key.push_back(k[i]);
     }
+}
+
+//encode functions
+char encode_text(char (&matrix)[lin][col], int pos, vector<char>& text, vector<char>& key, vector<char>& chars){
+    int colum,line;
+    for(int j = 0; j < col; j++){
+        if(matrix[0][j] == text[pos]){
+            colum = j;
+            break;
+        }
+    }
+    for(int j = 0; j < chars.size(); j++){
+        if(matrix[j][0] == key[pos]){
+            line = j;
+            break;
+        }
+    }
+    return matrix[line][colum];
+}
+
+
+int main(){
+    char matrix[lin][col];
+    vector<char> chars({'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
+                       'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'});
+
+    
+    //init 
+    matrix_fill(matrix, chars);
+    matrix_print(matrix);
+
+    //inputs
+    vector<char>key({});
+    vector<char>text({});
+
+    string t;
+    cout << "digite o que deseja codificar: " << endl;
+    cin >> t;
+    toUpperString(t);
+
+    string k;
+    cout << "digite a sua chave: *deve conter o mesmo numero de caracteres que a sua palavra/frase" << endl;
+    cin >> k;   
+    toUpperString(k);           
+    //end
+
+    string_separate(text,t,key,k);
 
     //encode
     vector<char>encode({});
+    int i = 0;
 
-    int a,b;
-
-    for(int i = 0; i < text.size(); i++){
-        for(int j = 0; j < chars.size(); j++){
-            if(matrix[0][j] == text[i]){
-                a = j;
-                break;
-            }
-        }
-        for(int j = 0; j < chars.size(); j++){
-             if(matrix[j][0] == key[i]){
-                b = j;
-                break;
-            }
-        }
-        encode.push_back(matrix[b][a]);
-    } 
-
-    for(int i = 0; i < encode.size(); i++){
-        cout << encode[i];
+    while (i < text.size())
+    {
+        encode.push_back(encode_text(matrix, i, text, key, chars));
+        i++;
     }
-
+    
+    vector_print(encode);
+    //end
 }
 
 
